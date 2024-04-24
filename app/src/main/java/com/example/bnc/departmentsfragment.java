679038ffed1daff3ed7ec84c44f599248920c067
Pragya <pragyakumari0624@gmail.com>
2024-04-24 19:23:10 +0530

    Modified physics fragment.

--- app/src/main/java/com/example/bnc/departmentsfragment.java
+++ app/src/main/java/com/example/bnc/departmentsfragment.java
@@ -48,50 +48,22 @@ public class departmentsfragment extends Fragment {
 
         textview1.setOnClickListener(v -> {
             hideOtherViews(gridLayout);
-//            textview1.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-//            property(textview1);
-//            setTextViewProperties(textview1);
-//            property3(textview1);
-//            setTextViewMargins(textview1, 0, 0);
             setMenu(R.menu.science_course);
         });
 
         textview2.setOnClickListener(v -> {
             hideOtherViews(gridLayout);
-//            textview2.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-//            property(textview2);
-//            setTextViewProperties(textview2);
-//            property3(textview2);
-//            setTextViewMargins(textview2, 0, 0);
             setMenu(R.menu.language_course);
         });
         textview3.setOnClickListener(v -> {
             hideOtherViews(gridLayout);
-//            textview3.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-//            property(textview3);
-//            setTextViewProperties(textview3);
-//            property2(textview3);
-//            setTextViewMargins(textview3, 0, 600);
             setMenu(R.menu.social_science_course);
         });
         textview4.setOnClickListener(v -> {
             hideOtherViews(gridLayout);
-//            textview4.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-//            property(textview4);
-//            setTextViewProperties(textview4);
-//            property2(textview4);
-//            setTextViewMargins(textview4, 0, 600);
             setMenu(R.menu.vocational_course);
         });
         frameLayout.setOnClickListener(v -> new Handler().postDelayed(() -> {
-//            gridLayout.setVisibility(View.VISIBLE);
-//            resetTextViewProperties(textview1);
-//            resetTextViewProperties(textview2);
-//            resetTextViewProperties(textview3);
-//            resetTextViewProperties(textview4);
-//            textview1.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.science_drawable, 0, 0); // Left, Top, Right, Bottom
-//            textview3.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.social_science_drawable, 0, 0);
-//            textview4.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.vocational_drawable, 0, 0);
             showOtherViews(gridLayout);
         }, 100));
 
@@ -102,6 +74,16 @@ public class departmentsfragment extends Fragment {
                 showOtherViews(gridLayout);
                 Toast.makeText(rootView.getContext(), "Physics Clicked", Toast.LENGTH_SHORT).show();
                 replaceFragment(new physicsfragment());
+            }
+            if(itemId == R.id.menu_chemistry){
+                showOtherViews(gridLayout);
+                Toast.makeText(rootView.getContext(), "Chemistry Clicked", Toast.LENGTH_SHORT).show();
+                replaceFragment(new chemistryFragment());
+            }
+            if(itemId == R.id.menu_mathematics){
+                showOtherViews(gridLayout);
+                Toast.makeText(rootView.getContext(), "Mathematics Clicked", Toast.LENGTH_SHORT).show();
+                replaceFragment(new mathematicsFragment());
             }
             return true;
         });
@@ -119,69 +101,6 @@ public class departmentsfragment extends Fragment {
 
     }
 
-    //    public void property(View text) {
-//        ViewGroup.LayoutParams layoutParams = text.getLayoutParams();
-//        layoutParams.width = 1200;
-//        layoutParams.height = 1400;
-//        text.setLayoutParams(layoutParams);
-//        GridLayout.LayoutParams Params = (GridLayout.LayoutParams) text.getLayoutParams();
-//        Params.setGravity(Gravity.CENTER);
-//        text.setLayoutParams(Params);
-//    }
-
-//    private void setTextViewProperties(TextView textView) {
-//        textView.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
-//        ViewGroup.LayoutParams layoutParams = textView.getLayoutParams();
-//        layoutParams.width = 1200;
-//        layoutParams.height = 1400;
-//        textView.setLayoutParams(layoutParams);
-////        GridLayout.LayoutParams params = (GridLayout.LayoutParams) textView.getLayoutParams();
-////        params.setGravity(Gravity.CENTER);
-////        textView.setLayoutParams(params);
-//    }
-
-//    public void property2(View text) {
-//        ViewGroup.MarginLayoutParams layoutParams1 = (ViewGroup.MarginLayoutParams) text.getLayoutParams();
-//        layoutParams1.setMargins(0, 0, 0, 600);
-//        text.setLayoutParams(layoutParams1);
-//    }
-
-    //    public void property3(View text) {
-//        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) text.getLayoutParams();
-//        layoutParams.setMargins(0, 100, 0, 0);
-//        text.setLayoutParams(layoutParams);
-//    }
-//    private void setTextViewMargins(TextView textView, int top, int bottom) {
-//        ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) textView.getLayoutParams();
-//        int screenWidth = getResources().getDisplayMetrics().widthPixels;
-//        int textViewWidth = 1200; // Width specified in dp
-//        int horizontalMargin = (screenWidth - textViewWidth) / 2;
-//        layoutParams.setMargins(horizontalMargin / 4, top, horizontalMargin, bottom);
-//        textView.setLayoutParams(layoutParams);
-//    }
-
-
-//    public void revert(View text_view) {
-//
-//        ViewGroup.LayoutParams layoutParams = text_view.getLayoutParams();
-//        layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
-//        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-//        text_view.setLayoutParams(layoutParams);
-//        ViewGroup.MarginLayoutParams layoutParams1 = (ViewGroup.MarginLayoutParams) text_view.getLayoutParams();
-//        layoutParams1.setMargins(0, 0, 0, 0);
-//        text_view.setLayoutParams(layoutParams1);
-//    }
-
-//    private void resetTextViewProperties(TextView textView) {
-//        ViewGroup.LayoutParams layoutParams = textView.getLayoutParams();
-//        layoutParams.width = ViewGroup.LayoutParams.WRAP_CONTENT;
-//        layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-//        textView.setLayoutParams(layoutParams);
-//        ViewGroup.MarginLayoutParams marginLayoutParams = (ViewGroup.MarginLayoutParams) textView.getLayoutParams();
-//        marginLayoutParams.setMargins(0, 0, 0, 0);
-//        textView.setLayoutParams(marginLayoutParams);
-//    }
-
     private void hideOtherViews(GridLayout gridLayout) {
         for (int i = 0; i < gridLayout.getChildCount(); i++) {
             View child = gridLayout.getChildAt(i);
@@ -189,7 +108,6 @@ public class departmentsfragment extends Fragment {
                 child.setVisibility(View.GONE);
             }
         }
-//        setTextViewProperties(textViewToShow);
     }
 
     private void setMenu(@MenuRes int menuResId) {
@@ -203,7 +121,6 @@ public class departmentsfragment extends Fragment {
 
         // Use the correct container ID where you want to replace the fragment
         fragmentTransaction.replace(R.id.frameLayout, fragment);
-
         fragmentTransaction.commit();
     }
 }
